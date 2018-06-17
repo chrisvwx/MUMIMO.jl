5d7a253d0933b8ffdda07783884c7efdf298f4ee
Christian Peel <chris.peel@ieee.org>
2018-06-16 19:53:17 -0700

    Update for 0.6.3

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,12 @@
 # --------------
 # Initialization
 # --------------
+Pkg.add("LLLplus")
 if VERSION<=v"0.6.3"
     include("../src/MUMIMO.jl")
 else
     using Pkg
 end
-Pkg.add("LLLplus")
 Pkg.add("PyPlot")
 
 using MUMIMO
@@ -16,5 +16,4 @@ using PyPlot
 # --------------
 # Basic test
 # --------------
-#mimoUplink(1,4,[2],[2],[0],[12],1,[30.0;])
 mimoUplink(100,4,[4],[4],[0],[12],12,[-5.0:5:20;])
